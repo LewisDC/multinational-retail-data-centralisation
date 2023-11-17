320c79a8e18298b90e7de3a534d7cde884309a2e
Lewis Crowther <l3wisdc@hotmail.com>
2023-11-17 22:38:34 +0000

    All data extracted and cleaned

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import re
 from datetime import datetime
+from dateutil.parser import parse
 
 class DataCleaning:
 
@@ -23,8 +25,7 @@ class DataCleaning:
         #  Establish valid country codes
         if valid_country_code is None:
             valid_country_code = ('GB', 'DE', 'US')
-        # The 'country' and 'country_code' columns are converted to categorical dtypes.
-        df['country'] = df['country'].astype('category')
+        # The 'country_code' column is converted to categorical dtypes.
         df['country_code'] = df['country_code'].astype('category')
         # The 'country_code' column is checked for invalid values, such as 'GGB', and replaced with 'GB'.
         df['country_code'] = df['country_code'].replace('GGB', 'GB')
@@ -33,16 +34,13 @@ class DataCleaning:
         return df.drop(incorrect_codes.index)
 
     @staticmethod
-    def convert_dates(df, date_columns=None):
-        if date_columns is None:
-            date_columns = ['date_of_birth', 'join_date']
+    def convert_dates(df, date_columns):
         # Convert date columns to datetime objects
         for col in date_columns:
             df[col] = pd.to_datetime(df[col], format='%Y-%m-%d', errors='ignore')
             df[col] = pd.to_datetime(df[col], format='%Y %B %d', errors='ignore')
             df[col] = pd.to_datetime(df[col], format='%B %Y %d', errors='ignore')
-            df[col] = pd.to_datetime(df[col], format='%Y/%m/%d', errors='ignore')
-            df[col] = pd.to_datetime(df[col], errors='coerce')
+            df[col] = pd.to_datetime(df[col], errors='coerce').dt.date
         return df
 
     @staticmethod
@@ -84,6 +82,8 @@ class DataCleaning:
                 'company': 'string',
                 'email_address': 'string',
                 'address': 'string',
+                'country': 'category',
+                'country_code': 'category',
                 'phone_number': 'string',
                 'user_uuid': 'string'
                 }
@@ -96,37 +96,122 @@ class DataCleaning:
             else:
                 print(f"Column '{col}' not found in the DataFrame.")
         return df
+    
+    @staticmethod
+    def convert_product_weights(weight_str):
+        weight_str = str(weight_str)
+        if 'x' in weight_str:
+            weight_str = weight_str.replace('g', '')
+            factors = weight_str.split(' x ')
+            return float(factors[0]) * float(factors[1]) / 1000
+        elif weight_str.endswith('.'):
+            weight_str = weight_str.replace(' .', '')
+            return weight_str
+        elif weight_str.endswith('kg'):
+            weight_str = weight_str.replace('kg', '')
+            return float(weight_str)
+        elif weight_str.endswith('g'):
+            weight_str = weight_str.replace('g', '')
+            return float(weight_str) / 1000
+        elif weight_str.endswith('ml'):
+            weight_str = weight_str.replace('ml', '')
+            return float(weight_str) / 1000
+        elif weight_str.endswith('oz'):
+            weight_str = weight_str.replace('oz', '')
+            return float(weight_str) * 0.0283495
+        else: 
+            print(f'{weight_str} not included')
+            return weight_str
 
     
     @staticmethod
     def clean_user_data(df):
+        column_data_types = {
+            'first_name': 'string',
+            'last_name': 'string',
+            'company': 'string',
+            'email_address': 'string',
+            'address': 'string',
+            'country': 'category',
+            'country_code': 'category',
+            'phone_number': 'string',
+            'user_uuid': 'string'
+            }
+        df = DataCleaning.convert_data_types(df, column_data_types)
         df = DataCleaning.remove_null_values(df)
         df = DataCleaning.remove_duplicates(df)
         df = DataCleaning.clean_country_data(df)
         df = DataCleaning.clean_email_addresses(df)
-        df = DataCleaning.convert_dates(df)
-        df = DataCleaning.clean_addresses(df)
-        df = DataCleaning.convert_data_types(df)
+        date_columns = ['date_of_birth', 'join_date']
+        df = DataCleaning.convert_dates(df, date_columns)
+        df = DataCleaning.clean_addresses(df)        
         df = DataCleaning.clean_phone_numbers(df)
         return df
     
     @staticmethod
-    def clean_card_data(df2):
-        # 
-        df2 = df2.drop_duplicates(keep=False)
-        df2['card_number'] = df2['card_number'].str.replace('?', '')
-        df2 = df2[~df2['card_number'].str.contains('[a-zA-Z?]', na=False)]
-        df2 = DataCleaning.convert_dates(df2, ['date_payment_confirmed'])
-        df2 = df2.dropna()
-        column_data_types = {
-            'card_number': 'string',
-            'expiry_date': 'string',
-            'card_provider': 'string'            
-        }
-        df2 = DataCleaning.convert_data_types(df2, column_data_types)
-        return df2
+    def clean_card_data(df): 
+        df = df.drop_duplicates(keep=False)
+        df['card_number'] = df['card_number'].str.replace('?', '')
+        df = df[~df['card_number'].str.contains('[a-zA-Z?]', na=False)]
+        df['date_payment_confirmed'] = df['date_payment_confirmed'].apply(parse).dt.date
+        df = df.dropna()
+        df['card_number'] = df['card_number'].astype('int64')
+        return df
 
-        
+    @staticmethod
+    def clean_store_data(df):
+        df = df.drop(columns=['lat'])
+        df = DataCleaning.clean_country_data(df)
+        df['address'] = df['address'].str.replace("\n", ', ')
+        df['continent'] = df['continent'].str.replace('ee', '')
+        df['longitude'] = df['longitude'].str.replace('N/A', 'NaN')
+        df['opening_date'] = df['opening_date'].apply(parse).dt.date
+        df['staff_numbers'] = df['staff_numbers'].replace('[a-zA-Z]', '', regex=True)
+        column_data_types = {
+            'longitude': 'float64',
+            'latitude': 'float64',
+            'staff_numbers': 'int16'
+        }
+        df = DataCleaning.convert_data_types(df, column_data_types)
+        return df
+    
+    @staticmethod
+    def clean_products_data(df):
+        df.rename(columns={'Unnamed: 0': 'index'}, inplace=True)
+        correct_spelling = lambda x: 'Still_available' if x == 'Still_avaliable' else x
+        df['removed'] = df['removed'].apply(correct_spelling)
+        valid_removed = ('Still_available', 'Removed')
+        df.drop(df[~df['removed'].isin(valid_removed)].index, inplace=True)
+        df['product_price'] = df['product_price'].str.replace('£', '')
+        df['weight'] = df['weight'].apply(DataCleaning.convert_product_weights)
+        df.rename(columns={'weight': 'weight(kg)', 'product_price': 'product_price(£)'}, inplace=True)
+        df['date_added'] = df['date_added'].apply(parse).dt.date
+        column_data_types = {
+            'product_price(£)': 'float64',
+            'weight(kg)': 'float64'            
+        }
+        df = DataCleaning.convert_data_types(df, column_data_types)
+        return df
+    
+    @staticmethod
+    def clean_orders_data(df):
+        df.drop(columns=['first_name', 'last_name', '1'], inplace=True)
+        df.rename(columns={'level_0': 'index'}, inplace=True)
+        return df
+    
+    @staticmethod
+    def clean_date_events_data(df):
+        valid_time_periods = ['Evening', 'Midday', 'Morning', 'Late_Hours']
+        df.drop(df[~df['time_period'].isin(valid_time_periods)].index, inplace=True)
+        df['timestamp'] = df['timestamp'].apply(parse).dt.time
+        column_data_types = {
+            'time_period': 'category',
+            'month': 'int8',
+            'year': 'int16',
+            'day': 'int8'
+        }
+        df = DataCleaning.convert_data_types(df, column_data_types)
+        return df
         
 
 
--- data_extraction.py
+++ data_extraction.py
@@ -2,6 +2,9 @@ import pandas as pd
 from database_utils import DatabaseConnector
 from data_cleaning import DataCleaning
 import tabula as tb
+import requests
+import json
+import boto3
 
 class DataExtractor:
     def __init__(self, db_connector):
@@ -16,16 +19,6 @@ class DataExtractor:
             print(f"Error reading table {table_name}: {e}")
             return None
 
-    def upload_dim_users():
-        yaml_file_path = 'db_creds.yaml'
-        db_connector = DatabaseConnector(yaml_file_path)
-        table_names = db_connector.list_db_tables(db_connector.engine)
-        df = DataExtractor(db_connector).read_rds_table(table_names[1])
-        print(df.head())
-        cleaned_df = DataCleaning.clean_user_data(df)
-        table_name = 'dim_users'
-        db_connector.upload_to_db(cleaned_df, table_name)
-
     def retrieve_pdf_data(link=None):
         if link == None:
             link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
@@ -34,11 +27,80 @@ class DataExtractor:
         pdf_df.head()
         return pdf_df    
     
-    def upload_dim_card_details():
-        yaml_file_path = 'db_creds.yaml'
-        db_connector = DatabaseConnector(yaml_file_path)
-        df2 = DataExtractor.retrieve_pdf_data()
-        cleaned_df2 = DataCleaning.clean_card_data(df2)
-        table_name = 'dim_card_details'
-        db_connector.upload_to_db(cleaned_df2, table_name)
+    def list_number_of_stores(self, return_number_stores_endpoint, header):
+        response = requests.get(return_number_stores_endpoint, headers=header)
+        number_stores = response.json()
+        number_of_stores = number_stores['number_stores']
+        return number_of_stores
+
+    def retrieve_stores_data(self, number_of_stores, endpoint, header):
+        data = []
+        for store in range(number_of_stores):
+            store_url = f'{endpoint}{store}'
+            response = requests.get(store_url, headers=header)
+            if response.status_code == 200:
+                store_data = pd.json_normalize(response.json())
+                data.append(store_data)
+
+            df = pd.concat(data).set_index('index')
+        print('Data retieval complete.')
+        return df          
+    
+    def extract_from_s3(self, s3_address):
+        s3 = boto3.client('s3')
+        bucket, key = s3_address.split('//')[1].split('/', 1)
+        response = s3.get_object(Bucket=bucket, Key=key)
+        df = pd.read_csv(response['Body'])
+        return df
+    
+
+if __name__=="__main__":
+    yaml_file_path = 'db_creds.yaml'
+
+    db_connector = DatabaseConnector(yaml_file_path)
+    extractor = DataExtractor(db_connector)
+    
+    creds = db_connector.read_db_creds(yaml_file_path)
+    table_names = db_connector.list_db_tables(db_connector.engine)
+
+    header = {creds['KEY']: creds['VALUE']}
+    retrieve_a_store_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/'
+    return_number_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+      
+    ### users details
+    users_df = DataExtractor(db_connector).read_rds_table(table_names[1])
+    cleaned_users_df = DataCleaning.clean_user_data(users_df)
+    
+    db_connector.upload_to_db(cleaned_users_df, table_name='dim_users')
+
+    ### Card details
+    card_details_df = DataExtractor.retrieve_pdf_data()
+    cleaned_card_details_df = DataCleaning.clean_card_data(card_details_df)
+    db_connector.upload_to_db(cleaned_card_details_df, table_name='dim_card_details')
+
+    ### stores
+    number_of_stores = extractor.list_number_of_stores(return_number_stores_endpoint, header)
+    stores_df = extractor.retrieve_stores_data(number_of_stores, retrieve_a_store_endpoint, header)
+
+    cleaned_stores_df = DataCleaning.clean_store_data(stores_df)
+    db_connector.upload_to_db(cleaned_stores_df, table_name='dim_store_details')
+
+    ### Products
+    s3_address = 's3://data-handling-public/products.csv'
+    product_data_df = extractor.extract_from_s3(s3_address)
+
+    cleaned_product_data_df = DataCleaning.clean_products_data(product_data_df)
+    db_connector.upload_to_db(cleaned_product_data_df, table_name='dim_products')
+    
+    ### Orders
+    orders_data_df = extractor.read_rds_table('orders_table')
+
+    cleaned_orders_data_df = DataCleaning.clean_orders_data(orders_data_df)
+    db_connector.upload_to_db(orders_data_df, table_name='orders_table')
+
+    ### Date events
+    date_events_data_df = pd.read_json('https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json')
+    
+    cleaned_date_events_data_df = DataCleaning.clean_date_events_data(date_events_data_df)
+    db_connector.upload_to_db(cleaned_date_events_data_df, table_name='dim_date_times')
 
